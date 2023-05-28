2b553344cf0a0f103e6abad4f0b8a20ef46088ee
Igor Falcao <igoraaf1@gmail.com>
2023-05-28 19:35:40 -0300

    correcao de bug API

--- Front-END/src/hooks/useApiLogin.ts
+++ Front-END/src/hooks/useApiLogin.ts
@@ -4,7 +4,7 @@ import axios from 'axios';
 // CUSTOM HOOK PARA REALIZACAO DE REQUISICOES RELACIONADAS AO USUARIO
 
 const api = axios.create({
-    baseURL: 'https://api-todolist-37w4.onrender.com/'
+    baseURL: 'http://localhost:3001'
 });
 
 
--- Front-END/src/hooks/useApiTask.ts
+++ Front-END/src/hooks/useApiTask.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'https://api-todolist-37w4.onrender.com/'
+    baseURL: 'http://localhost:3001'
 });
 
 
